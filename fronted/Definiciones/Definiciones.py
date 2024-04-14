336e636abf633cf01e49b78b4f77a64345084514
wiesnerabril <132773042+wiesnerabril@users.noreply.github.com>
2024-04-14 18:46:59 -0500

    actualizacion

--- fronted/Definiciones/Definiciones.py
+++ fronted/Definiciones/Definiciones.py
@@ -5,6 +5,7 @@ import dash_bootstrap_components as dbc
 Definiciones = dbc.Container([
     dbc.Row([
         dbc.Col('Conceptos', md= 9, style={'background-color':'Blue'}),
+        html.Br(), html.Br(),
         dbc.Col('Imagen Ilustrativa', md= 3, style={'background-color':'yellow'}), 
         html.Br(), html.Br(),
     ])
--- fronted/fronted.py
+++ fronted/fronted.py
@@ -5,9 +5,10 @@ import dash_bootstrap_components as dbc
 from fronted.Definiciones.Definiciones import Definiciones
 
 layout = dbc.Container([
-    dbc.Col('Central',md=12,style={'background-color':'red'}),
-    dbc.Col('Definiciones',md=12,style={'background-color':'red'}),
-    dbc.Col('Inferior',md=12,style={'background-color':'red'}),
     dbc.Col('Superior1',md=12,style={'background-color':'red'}),
+    dbc.Col(Definiciones,md=12,style={'background-color':'red'}),
+    dbc.Col('Central',md=12,style={'background-color':'red'}),
+    dbc.Col('Inferior',md=12,style={'background-color':'red'}),
+    
 
 ])
