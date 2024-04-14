061e1be6bd62c125b7592088f8165ca4a8cbec0f
wiesnerabril <132773042+wiesnerabril@users.noreply.github.com>
2024-04-14 17:26:41 -0500

    modificcion caretas

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -3,16 +3,4 @@ Nycole Valentina Wiesner Cod. 20222579031
 Angie Marcela Varela Cod. 20222579035
 """
 
-a=6
-b=5
-c=a+b
-
-print(c);
-
-min(5, 8, 10, 20)
-<<<<<<< HEAD
-
-#HOLA 
-=======
-print(min);
->>>>>>> 9815ee131f43dd1579d6ea263e87a227cf695235
+import dash
