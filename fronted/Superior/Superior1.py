92fbe539a6909076082eed1766affa6f0dda4c26
wiesnerabril <132773042+wiesnerabril@users.noreply.github.com>
2024-04-14 19:13:28 -0500

    super

---
+++ fronted/Superior/Superior1.py
@@ -0,0 +1,10 @@
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+
+Superior1 = dbc.Container([
+    dbc.Row([
+        dbc.Col('Diseña cimetación',md=8,style={'background-color':'blue'}),
+        dbc.Col('Logo',md=4),
+    ])
+])
