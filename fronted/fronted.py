41b26041fa98ac557db0cd291be2207173e47169
wiesnerabril <132773042+wiesnerabril@users.noreply.github.com>
2024-04-14 19:17:37 -0500

    ff

--- fronted/fronted.py
+++ fronted/fronted.py
@@ -3,9 +3,10 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 from fronted.Definiciones.Definiciones import Definiciones
+from fronted.Superior.Superior1 import Superior1
 
 layout = dbc.Container([
-    dbc.Col('Superior1',md=12,style={'background-color':'red'}),
+    dbc.Col(Superior1,md=12,style={'background-color':'red'}),
     dbc.Col(Definiciones,md=12,style={'background-color':'red'}),
     dbc.Col('Central',md=12,style={'background-color':'red'}),
     dbc.Col('Inferior',md=12,style={'background-color':'red'}),
