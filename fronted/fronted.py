c2f9d163f1176d514ecae877ee7b1b7bff4404b2
MarcelaVarela1 <132773003+MarcelaVarela1@users.noreply.github.com>
2024-04-18 19:53:19 -0500

    ActualizacionDefiniciones

--- fronted/fronted.py
+++ fronted/fronted.py
@@ -4,14 +4,14 @@ import dash_bootstrap_components as dbc
 
 from fronted.Definiciones.Definiciones import Definiciones
 from fronted.Superior.Superior1 import Superior1
-from fronted.Central.Central import Central
 from fronted.Inferior.Inferior import Inferior
+from fronted.Central.Central import Central
+
 
 layout = dbc.Container([
-    dbc.Col(Superior1,md=12,style={'background-color':'red'}),
-    dbc.Col(Definiciones,md=12,style={'background-color':'red'}),
+    dbc.Col(Superior1 ,md=12,style={'background-color':'red'}),
+    dbc.Col(Definiciones ,md=12,style={'background-color':'red'}),
     dbc.Col(Central,md=12,style={'background-color':'red'}),
-    dbc.Col(Inferior,md=12,style={'background-color':'red'}),
-    
-
-])
+    dbc.Col(Inferior ,md=12,style={'background-color':'red'}),
+    dbc.Col(Superior1 ,md=12,style={'background-color':'red'}),
+])  
