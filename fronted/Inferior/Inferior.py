26a9e004f39142fae6de510367ed5b3d49159bb6
wiesnerabril <132773042+wiesnerabril@users.noreply.github.com>
2024-04-19 23:16:23 -0500

    front

--- fronted/Inferior/Inferior.py
+++ fronted/Inferior/Inferior.py
@@ -6,30 +6,44 @@ Inferior = dbc.Container([
     dbc.Row([
         dbc.Col(
             html.Div([
+
             html.H2("Toma de Datos"),
-            html.P("A. Cohesión (c)  [N/m^2]"),
-        dcc.Input(type='number' , value= "0", id= "entrada"),
+                html.Label('A. Cohesión (c)  [N/m^2]'),
+                dcc.Input(type='number' , value= "0", id= "entradacohesión"),
+                html.Label(id = 'Salidacohesión'),
+                html.Br(),
 
-            html.P("B. Sobrecarga (q)  [N]"),
-        dcc.Input(type='number' , value= "0", id= "entrada"),
+            
+                html.Label('B. Sobrecarga (q)  [N]'), 
+                dcc.Input(type='number' , value= "0", id= "entradasc"),
+                html.Label(id = 'Salidasc'),
+                html.Br(),
 
-            html.P("C. Base (B)  [m]"),
-        dcc.Input(type='number' , value= "0", id= "entrada"),
-    
-            html.P("D. Peso específico (γ)  [Kg/m^3]"),
-        dcc.Input(type='number' , value= "0", id= "entrada"),
-    
-             html.P("E. Ángulo de Ficción (Φ)  [Kg/m^3]"),
-        dcc.Input(type='number' , value= "0", id= "entrada"),
+                html.Label('C. Base       (B)  [m]'),
+                dcc.Input(type='number' , value= "0", id= "entradabase"),
+                html.Label(id = 'Salidabase'),
+                html.Br(),
 
+                html.Label('D. Peso específico (γ) [Kg/m^3]'), 
+                dcc.Input(type='number' , value= "0", id= "entradape"),
+                html.Label(id = 'Salidape'),
+                html.Br(),
+
+                html.Label('E. Ángulo de Ficción (Φ) [°]'), 
+                dcc.Input(type='number' , value= "0", id= "entradaaf"),
+                html.Label(id = 'Salidaaf'),
+                html.Br(),
 
             ])
             
-            , md= 4, style={'background-color':'#FFF0F5'}),
+            , md= 7, style={'background-color':'#FFF0F5'}),
         html.Br(), html.Br(),
+
+
         dbc.Col('Resultado q', md= 5, style={'background-color':'#FFF0F5'}), 
-        html.Br(), html.Br(),
-        dbc.Col('Imagen resultado', md= 3, style={'background-color':'#FFF0F5'}), 
-        html.Br(), html.Br(),
+
+
+
+    
     ])
 ])
