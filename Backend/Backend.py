3d6b38e44d58fe0d33c2326c24f62a8ce7a1cd5c
MarcelaVarela1 <132773003+MarcelaVarela1@users.noreply.github.com>
2024-04-19 23:35:30 -0500

    Ajuste+

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -4,3 +4,14 @@ Angie Marcela Varela Cod. 20222579035
 """
 
 import dash
+from dash import html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import math
+
+@app.callback(
+    Output('lo que se ponga en output','Children'),
+    Input('lo que se puso en input', 'value')
+)
+def CargaUltima('lo que se puso en input'):
+    calculo = 
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import dash
 from dash import html
 import dash_bootstrap_components as dbc
+import math
+from dash.dependencies import Input, Output
 
 
 #se importa el front 
